76dd20dac28bafbb7155cc2c77f935f050dbaa94
Karen - iMac <kellsworth628@gmail.com>
2021-06-04 09:54:51 -0600

    updates

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-// import { HashRouter } from 'react-router-dom';
-import { BrowserRouter, HashRouter } from 'react-router-dom';
+import { HashRouter, BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 
